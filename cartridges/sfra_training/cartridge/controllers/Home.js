b4a879204db0da58d0d38f65a4198c4861ffa4c2
Shubham Agrawal <shubham241999@gmail.com>
2022-11-17 20:11:59 +0530

    Use Prepend, Append and Replace to Extend a Controller

--- cartridges/sfra_training/cartridge/controllers/Home.js
+++ cartridges/sfra_training/cartridge/controllers/Home.js
@@ -1,49 +1,32 @@
-'use strict';
+"use strict";
+var server = require("server");
+var cache = require("*/cartridge/scripts/middleware/cache");
 
-/**
- * @namespace Home
- */
+server.extend(module.superModule);
+server.prepend("Show", cache.applyDefaultCache, function (req, res, next) {
+  var viewData = res.getViewData();
+  viewData.param1 = "This is from prepend";
+  res.setViewData(viewData);
+  next();
+});
+server.append("Show", cache.applyDefaultCache, function (req, res, next) {
+  var viewData = res.getViewData();
+  //declare param1 as a variable that equals 'General company details.'
+  var appendParam = "This is from append";
+  var queryparam = req.querystring.param
+    ? req.querystring.param
+    : "no parameter was passed";
 
-var server = require('server');
-var cache = require('*/cartridge/scripts/middleware/cache');
-var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
-var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
-
-/**
- * Any customization on this endpoint, also requires update for Default-Start endpoint
- */
-/**
- * Home-Show : This endpoint is called when a shopper navigates to the home page
- * @name Base/Home-Show
- * @function
- * @memberof Home
- * @param {middleware} - consentTracking.consent
- * @param {middleware} - cache.applyDefaultCache
- * @param {category} - non-sensitive
- * @param {renders} - isml
- * @param {serverfunction} - get
- */
-server.get('Show', consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
-    var Site = require('dw/system/Site');
-    var PageMgr = require('dw/experience/PageMgr');
-    var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
-
-    pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
-
-    var page = PageMgr.getPage('homepage');
-
-    if (page && page.isVisible()) {
-        res.page('homepage');
-    } else {
-        res.render('home/homePage');
-    }
-    next();
-}, pageMetaData.computedPageMetaData);
-
-server.get('ErrorNotFound', function (req, res, next) {
-    res.setStatusCode(404);
-    res.render('error/notFound');
-    next();
+  //Here grab whatever prepend added to viewData + the message here + the optional query string param
+  res.setViewData({
+    param1:
+      viewData.param1 +
+      " AND " +
+      appendParam +
+      " AND querystring param = " +
+      queryparam,
+  });
+  next();
 });
 
 module.exports = server.exports();
