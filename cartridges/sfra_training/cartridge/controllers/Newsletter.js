6c47bbcb345b3e2a6f377a488fcd18129af6767e
Shubham Agrawal <118164511+agr-shubham-ps@users.noreply.github.com>
2022-11-19 14:46:29 +0530

    Display Data Submitted From a Form

--- cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -15,4 +15,30 @@ server.get("Show", server.middleware.https, function (req, res, next) {
   next();
 });
 
+server.post("Handler", server.middleware.https, function (req, res, next) {
+  var newsletterForm = server.forms.getForm("newsletter");
+  var continueUrl = dw.web.URLUtils.url("Newsletter-Show");
+
+  // Perform any server-side validation before this point, and invalidate form accordingly
+  if (newsletterForm.valid) {
+    // Send back a success status, and a redirect to another route
+    res.render("/newsletter/newslettersuccess", {
+      continueUrl: continueUrl,
+      newsletterForm: newsletterForm,
+    });
+  } else {
+    // Handle server-side validation errors here: this is just an example
+    res.render("/newsletter/newslettererror", {
+      errorMsg: dw.web.Resource.msg(
+        "error.crossfieldvalidation",
+        "newsletter",
+        null
+      ),
+      continueUrl: continueUrl,
+    });
+  }
+
+  next();
+});
+
 module.exports = server.exports();
