83f064f80ca99b855d0cebdc6c33d585ac9de153
Shubham Agrawal <118164511+agr-shubham-ps@users.noreply.github.com>
2022-11-19 15:44:46 +0530

    Save Form Data in a Custom Object Instance

--- cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -33,10 +33,30 @@ server.post(
     // Perform any server-side validation before this point, and invalidate form accordingly
     if (newsletterForm.valid) {
       // Show the success page
-      res.json({
-        success: true,
-        redirectUrl: URLUtils.url("Newsletter-Success").toString(),
-      });
+      var Transaction = require("dw/system/Transaction");
+      try {
+        Transaction.wrap(function () {
+          var CustomObjectMgr = require("dw/object/CustomObjectMgr");
+          var co = CustomObjectMgr.createCustomObject(
+            "NewsletterSubscription",
+            newsletterForm.email.value
+          );
+          co.custom.firstName = newsletterForm.fname.value;
+          co.custom.lastName = newsletterForm.lname.value;
+
+          res.json({
+            success: true,
+            redirectUrl: URLUtils.url("Newsletter-Success").toString(),
+          });
+        });
+      } catch (e) {
+        var err = e;
+        res.setStatusCode(500);
+        res.json({
+          error: true,
+          redirectUrl: URLUtils.url("Error-Start").toString(),
+        });
+      }
     } else {
       // Handle server-side validation errors here: this is just an example
       res.setStatusCode(500);
