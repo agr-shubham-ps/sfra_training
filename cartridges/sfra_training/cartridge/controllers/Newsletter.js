2869434cefa296ff6a862353e4416f3f4e83a2a8
Shubham Agrawal <118164511+agr-shubham-ps@users.noreply.github.com>
2022-11-19 15:05:52 +0530

    Implement CSRF on a Form

--- cartridges/sfra_training/cartridge/controllers/Newsletter.js
+++ cartridges/sfra_training/cartridge/controllers/Newsletter.js
@@ -2,42 +2,53 @@
 
 var server = require("server");
 var URLUtils = require("dw/web/URLUtils");
+var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
 
-server.get("Show", server.middleware.https, function (req, res, next) {
-  var actionUrl = dw.web.URLUtils.url("Newsletter-Handler");
-  var newsletterForm = server.forms.getForm("newsletter");
-  newsletterForm.clear();
+server.get(
+  "Show",
+  server.middleware.https,
+  csrfProtection.generateToken,
+  function (req, res, next) {
+    var actionUrl = dw.web.URLUtils.url("Newsletter-Handler");
+    var newsletterForm = server.forms.getForm("newsletter");
+    newsletterForm.clear();
 
-  res.render("/newsletter/newslettersignup", {
-    actionUrl: actionUrl,
-    newsletterForm: newsletterForm,
-  });
-
-  next();
-});
-
-server.post("Handler", server.middleware.https, function (req, res, next) {
-  var newsletterForm = server.forms.getForm("newsletter");
-  var continueUrl = dw.web.URLUtils.url("Newsletter-Show");
-
-  // Perform any server-side validation before this point, and invalidate form accordingly
-  if (newsletterForm.valid) {
-    // Show the success page
-    res.json({
-      success: true,
-      redirectUrl: URLUtils.url("Newsletter-Success").toString(),
+    res.render("/newsletter/newslettersignup", {
+      actionUrl: actionUrl,
+      newsletterForm: newsletterForm,
     });
-  } else {
-    // Handle server-side validation errors here: this is just an example
-    res.setStatusCode(500);
-    res.json({
-      error: true,
-      redirectUrl: URLUtils.url("Error-Start").toString(),
-    });
+
+    next();
   }
+);
 
-  next();
-});
+server.post(
+  "Handler",
+  server.middleware.https,
+  csrfProtection.validateAjaxRequest,
+  function (req, res, next) {
+    var newsletterForm = server.forms.getForm("newsletter");
+    var continueUrl = dw.web.URLUtils.url("Newsletter-Show");
+
+    // Perform any server-side validation before this point, and invalidate form accordingly
+    if (newsletterForm.valid) {
+      // Show the success page
+      res.json({
+        success: true,
+        redirectUrl: URLUtils.url("Newsletter-Success").toString(),
+      });
+    } else {
+      // Handle server-side validation errors here: this is just an example
+      res.setStatusCode(500);
+      res.json({
+        error: true,
+        redirectUrl: URLUtils.url("Error-Start").toString(),
+      });
+    }
+
+    next();
+  }
+);
 
 server.get("Success", server.middleware.https, function (req, res, next) {
   res.render("/newsletter/newslettersuccess", {
